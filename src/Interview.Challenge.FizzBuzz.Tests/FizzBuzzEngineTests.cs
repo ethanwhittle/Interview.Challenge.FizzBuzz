d831f086288e80d55c6a9fdf4c8da80e6fccf795
Ethan Whittle <ethanwhittle@hotmail.co.uk>
2024-02-06 20:55:57 +0000

    Fixed initial test

--- src/Interview.Challenge.FizzBuzz.Tests/FizzBuzzEngineTests.cs
+++ src/Interview.Challenge.FizzBuzz.Tests/FizzBuzzEngineTests.cs
@@ -6,6 +6,38 @@ using System.Threading.Tasks;
 
 namespace Interview.Challenge.FizzBuzz.Tests
 {
+    public class FizzBuzzEngine
+    {
+        private readonly IFizzBuzzRule[] _rules;
+
+        public FizzBuzzEngine(IFizzBuzzRule[] rules)
+        {
+            ArgumentNullException.ThrowIfNull(rules);
+
+            _rules = rules;
+        }
+
+        public string Apply(int number)
+        {
+            var result = string.Empty;
+
+            foreach (var rule in _rules)
+            {
+                if (rule.CheckRule(number))
+                {
+                    result += rule.Name;
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(result))
+            {
+                result = number.ToString();
+            }
+
+            return result;
+        }
+    }
+
     public class FizzBuzzEngineTests
     {
         [Theory]
@@ -13,9 +45,7 @@ namespace Interview.Challenge.FizzBuzz.Tests
         public void FizzBuzzEngine_Returns_Number_When_No_Rules_Apply(int value)
         {
             // Arrange
-            var rules = Substitute.For<IFizzBuzzRule[]>();
-
-            var engine = new FizzBuzzEngine(rules);
+            var engine = new FizzBuzzEngine([]);
 
             // Act
             var result = engine.Apply(value);
