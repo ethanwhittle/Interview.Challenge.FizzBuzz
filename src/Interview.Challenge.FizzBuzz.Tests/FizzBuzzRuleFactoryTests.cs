f0b2b2e6bf607668d8724c6b7971a3203ed514e1
Ethan Whittle <ethanwhittle@hotmail.co.uk>
2024-02-06 20:38:30 +0000

    Added extra tests

--- src/Interview.Challenge.FizzBuzz.Tests/FizzBuzzRuleFactoryTests.cs
+++ src/Interview.Challenge.FizzBuzz.Tests/FizzBuzzRuleFactoryTests.cs
@@ -33,19 +33,34 @@ namespace Interview.Challenge.FizzBuzz.Tests
 
     public class FizzBuzzRuleFactoryTests
     {
+        private readonly FizzBuzzRuleFactory _testClass;
+
+        public FizzBuzzRuleFactoryTests()
+        {
+            _testClass = new FizzBuzzRuleFactory();
+        }
+
         [Fact]
         public void GetRules_Returns_Rules()
         {
-            // Arrange
-            var factory = new FizzBuzzRuleFactory();
-
-            // Act
-            IFizzBuzzRule[] rules = factory.GetRules();
+            // Arrange / Act
+            IFizzBuzzRule[] rules = _testClass.GetRules();
 
             // Assert
             rules.Should().NotBeNull().And.HaveCount(2);
             rules[0].Should().BeOfType<FizzRule>();
             rules[1].Should().BeOfType<BuzzRule>();
+        }
+
+        [Fact]
+        public void Order_IsReadOnly()
+        {
+            // Act
+            var orderProperty = typeof(RuleOrderAttribute).GetProperty("Order");
+
+            // Assert
+            orderProperty.Should().NotBeNull();
+            orderProperty!.CanWrite.Should().BeFalse();
         }
     }
 }
