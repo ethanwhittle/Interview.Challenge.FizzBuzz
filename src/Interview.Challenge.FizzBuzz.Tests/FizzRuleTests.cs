745538883ad07ff204fdcb17616cec7b53f38462
Ethan Whittle <ethanwhittle@hotmail.co.uk>
2024-02-06 19:50:58 +0000

    Moar refactoring

--- src/Interview.Challenge.FizzBuzz.Tests/FizzRuleTests.cs
+++ src/Interview.Challenge.FizzBuzz.Tests/FizzRuleTests.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace Interview.Challenge.FizzBuzz.Tests
 {
-    public class FizzRule()
+    public interface IFizzBuzzRule
     {
+        string Name { get; }
+
+        bool CheckRule(int number);
+    }
+
+    public class FizzRule : IFizzBuzzRule
+    {
+        public string Name => "Fizz";
+
         public bool CheckRule(int number)
         {
             if (number % 3 == 0)
@@ -21,20 +30,41 @@ namespace Interview.Challenge.FizzBuzz.Tests
 
     public class FizzRuleTests
     {
+        private readonly FizzRule _testClass;
+
+        public FizzRuleTests()
+        {
+            _testClass = new FizzRule();
+        }
+
+        [Fact]
+        public void FizzRule_CanConstruct()
+        {
+            // Arrange
+            var instance = new FizzRule();
+
+            // Assert
+            instance.Should().NotBeNull();
+        }
+
         [Theory]
         [InlineData(3)]
         [InlineData(6)]
         [InlineData(9)]
         public void FizzRule_Returns_True_For_Multiples_Of_3(int number)
         {
-            // Arrange
-            var fizzRule = new FizzRule();
-
             // Act
-            var result = fizzRule.CheckRule(number);
+            var result = _testClass.CheckRule(number);
 
             // Assert
             result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void FizzRule_CanGetName()
+        {
+            // Assert
+            _testClass.Name.Should().NotBeNull().And.Be("Fizz");
         }
     }
 }
