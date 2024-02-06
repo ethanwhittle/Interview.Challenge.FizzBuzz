d8194fabc2c667478ae4b5a264c39c9956eae526
Ethan Whittle <ethanwhittle@hotmail.co.uk>
2024-02-06 19:35:52 +0000

    Multiples of 3 Start

---
+++ src/Interview.Challenge.FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.Challenge.FizzBuzz.Tests
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void FizzBuzz_Returns_Fizz_For_Multiples_Of_3()
+        {
+            // Arrange
+            var fizzBuzz = new FizzBuzz();
+
+            // Act
+            var result = fizzBuzz.GetFizzyBuzzy(3);
+
+            // Assert
+            result.Should().Be("Fizz");
+        }
+    }
+}
