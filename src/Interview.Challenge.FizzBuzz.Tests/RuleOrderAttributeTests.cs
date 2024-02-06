85c3cdf7bb9bbf00d88df9953dd2813174531e60
Ethan Whittle <ethanwhittle@hotmail.co.uk>
2024-02-06 20:20:35 +0000

    Working on rule order attribute

---
+++ src/Interview.Challenge.FizzBuzz.Tests/RuleOrderAttributeTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.Challenge.FizzBuzz.Tests
+{
+    public class RuleOrderAttributeTests
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void RuleOrderAttribute_CanBeConstructed(int value)
+        {
+            // Arrange / Act
+            var ruleOrderAttribute = new RuleOrderAttribute(value);
+
+            // Assert
+            ruleOrderAttribute.Order.ShouldBe(value);
+        }
+    }
+}
