8ab50b49d9f68ed8a707b833123c17e8ad49ed75
Ethan Whittle <ethanwhittle@hotmail.co.uk>
2024-02-06 20:07:08 +0000

    Began on rule factory

---
+++ src/Interview.Challenge.FizzBuzz.Tests/FizzBuzzRuleFactoryTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.Challenge.FizzBuzz.Tests
+{
+    public class FizzBuzzRuleFactoryTests
+    {
+        [Fact]
+        public void GetRules_Returns_Rules()
+        {
+            // Arrange
+            var factory = new FizzBuzzRuleFactory();
+
+            // Act
+            IFizzBuzzRule[] rules = factory.GetRules();
+
+            // Assert
+            rules.Should().NotBeNull().And.HaveCount(2);
+        }
+    }
+}
