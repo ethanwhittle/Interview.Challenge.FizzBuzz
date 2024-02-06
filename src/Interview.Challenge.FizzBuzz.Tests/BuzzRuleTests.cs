67cb5cfb77c4fce8f9154679b8dbea4ee6f107c4
Ethan Whittle <ethanwhittle@hotmail.co.uk>
2024-02-06 20:04:15 +0000

    Extrapolated and replicated for the Buzz rule

---
+++ src/Interview.Challenge.FizzBuzz.Tests/BuzzRuleTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Interview.Challenge.FizzBuzz.Tests
+{
+    public class BuzzRuleTests
+    {
+        private readonly BuzzRule _testClass;
+
+        public BuzzRuleTests()
+        {
+            _testClass = new BuzzRule();
+        }
+
+        [Fact]
+        public void BuzzRule_CanConstruct()
+        {
+            // Arrange
+            var instance = new BuzzRule();
+
+            // Assert
+            instance.Should().NotBeNull();
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(15)]
+        public void BuzzRule_Returns_True_For_Multiples_Of_5(int number)
+        {
+            // Act
+            var result = _testClass.CheckRule(number);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void BuzzRule_CanGetName()
+        {
+            // Assert
+            _testClass.Name.Should().NotBeNull().And.Be("Buzz");
+        }
+    }
+}
---
+++ src/Interview.Challenge.FizzBuzz/BuzzRule.cs
@@ -0,0 +1,23 @@
+﻿namespace Interview.Challenge.FizzBuzz
+{
+    /// <summary>
+    /// Represents a rule for the FizzBuzz game where numbers divisible by 5 are replaced with "Buzz".
+    /// </summary>
+    public class BuzzRule : IFizzBuzzRule
+    {
+        /// <summary>
+        /// Gets the name of the rule, which is "Buzz".
+        /// </summary>
+        public string Name => "Buzz";
+
+        /// <summary>
+        /// Checks if the given number is divisible by 5.
+        /// </summary>
+        /// <param name="number">The number to check.</param>
+        /// <returns>True if the number is divisible by 5, otherwise false.</returns>
+        public bool CheckRule(int number)
+        {
+            return number % 5 == 0;
+        }
+    }
+}
